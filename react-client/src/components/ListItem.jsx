50fae780f68aa1aa51c4a8752c882783867c8ea2
Hack Reactor Student <student@hackreactor.com>
2018-05-28 10:05:38 -0500

    Apple Music Logo fixed

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -9,7 +9,7 @@ const ListItem = (props) => (
     <td>{props.item.intensity}</td>
     <td>{props.item.wordiness}</td>
     <td>{props.item.danciness}</td>
-    <td><img src="Apple.jpg" /></td>
+    <td><img src="Apple.jpg" alt="Apple Music" width="120"  height="64"/></td>
   </tr>
 )
 
