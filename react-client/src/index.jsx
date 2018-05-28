ca6f5ce16caada4ab3ee72f729262f6e918ffd74
Hack Reactor Student <student@hackreactor.com>
2018-05-28 13:23:35 -0500

    Attempt to add button link

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -83,6 +83,9 @@ class App extends React.Component {
 /* Deleted HTML elements:
       <button onClick = {() => {console.log(this.state)}} >State</button>
       <img src="images.png">
+
+<a ng-href="https://www.facebook.com/v2.3/dialog/oauth?client_id=174829003346&amp;state=AQBk2gTpW97eSicf5juVO7EMH9R1e-nSssXUzav2hkw16vnBITPhrQAlpk7Mu_tDjgBCuxNqI0X3gjwR_di0uztuZmpuIPSmVMWsGgH_49bURZB8WBIMx7WkoVH-QVyBjg-C&amp;redirect_uri=https%3A%2F%2Faccounts.spotify.com%2Fapi%2Ffacebook%2Foauth%2Faccess_token" class="btn btn-sm btn-block btn-facebook ng-binding" target="_parent" role="button" href="https://www.facebook.com/v2.3/dialog/oauth?client_id=174829003346&amp;state=AQBk2gTpW97eSicf5juVO7EMH9R1e-nSssXUzav2hkw16vnBITPhrQAlpk7Mu_tDjgBCuxNqI0X3gjwR_di0uztuZmpuIPSmVMWsGgH_49bURZB8WBIMx7WkoVH-QVyBjg-C&amp;redirect_uri=https%3A%2F%2Faccounts.spotify.com%2Fapi%2Ffacebook%2Foauth%2Faccess_token">Log in with Facebook</a>
+
 */
 
   render () {
@@ -90,7 +93,7 @@ class App extends React.Component {
     return (
       <div>
         <div id="facebook">
-          <p>Log in with Facebook</p>
+          <a href="https://www.facebook.com/login.php?skip_api_login=1&api_key=174829003346&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fv2.3%2Fdialog%2Foauth%3Fredirect_uri%3Dhttps%253A%252F%252Faccounts.spotify.com%252Fapi%252Ffacebook%252Foauth%252Faccess_token%26state%3DAQBk2gTpW97eSicf5juVO7EMH9R1e-nSssXUzav2hkw16vnBITPhrQAlpk7Mu_tDjgBCuxNqI0X3gjwR_di0uztuZmpuIPSmVMWsGgH_49bURZB8WBIMx7WkoVH-QVyBjg-C%26client_id%3D174829003346%26ret%3Dlogin%26logger_id%3D11dc7753-bc52-9220-53a8-8f7118a8b750&cancel_url=https%3A%2F%2Faccounts.spotify.com%2Fapi%2Ffacebook%2Foauth%2Faccess_token%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3DAQBk2gTpW97eSicf5juVO7EMH9R1e-nSssXUzav2hkw16vnBITPhrQAlpk7Mu_tDjgBCuxNqI0X3gjwR_di0uztuZmpuIPSmVMWsGgH_49bURZB8WBIMx7WkoVH-QVyBjg-C%23_%3D_&display=page&locale=en_US&logger_id=11dc7753-bc52-9220-53a8-8f7118a8b750">Log in with Facebook</a>
         </div>
         <div>
         <h1> .
